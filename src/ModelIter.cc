b341539a5577d0a206494eef400d15300d407551
Carlos Agüero <caguero@osrfoundation.org>
2017-10-27 16:59:15 -0700

    Avoid segfault

--- src/ModelIter.cc
+++ src/ModelIter.cc
@@ -182,7 +182,7 @@ IterRESTIds::IterRESTIds(REST *_rest, ClientConfig *_config,
     // ToDo: resp.statusCode should return != 200 when the page requested does
     // not exist. When this happens we should stop without calling ParseModels()
     // https://bitbucket.org/ignitionrobotics/ign-fuelserver/issues/7
-    if (resp.data == "null\n")
+    if (resp.data == "null\n" || resp.statusCode != 200)
       break;
 
     // Parse the response.
@@ -193,6 +193,8 @@ IterRESTIds::IterRESTIds(REST *_rest, ClientConfig *_config,
       std::end(modelIds));
   } while (!modelIds.empty());
 
+  if (this->ids.empty())
+    return;
 
   this->idIter = this->ids.begin();
 
