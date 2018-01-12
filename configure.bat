0596e4a9058fac212c920527d5951f3f4b662e56
Steve Peters <scpeters@openrobotics.org>
2018-01-12 11:53:20 -0800

    update configure.bat

--- configure.bat
+++ configure.bat
@@ -1,23 +1,25 @@
-@rem Run CMake, pointing to sibling directories containing dependencies.
-@rem Note that zmq and cppzmq are relative to the source dir, while
-@rem protobuf is relative to the build dir.  Not sure why.
 
+:: NOTE: This script is only meant to be used as part of the ignition developers' CI system
+:: Users and developers should build and install this library using cmake and Visual Studio
+
+
+:: Install dependencies
+:: call %win_lib% :download_unzip_install dlfcn-win32-vc15-x64-dll-MD.zip
+call %win_lib% :install_ign_project ign-common1 ign-common1
+
+:: Set configuration variables
 @set build_type=Release
 @if not "%1"=="" set build_type=%1
+@echo Configuring for build type %build_type%
 
-@set build_bitness=64
-@if not "%2"=="" set build_bitness=%2
+:: Go to the directory that this configure.bat file exists in
+cd /d %~dp0
 
-@set IGNITION-MATH_PATH=%cd%\..\..\ign-math\build\install\%build_type%
-@set IGNITION-MATH_CMAKE_PREFIX_PATH=%IGNITION-MATH_PATH%\CMake
+:: Create a build directory and configure
+md build
+cd build
+cmake .. -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX="%WORKSPACE_INSTALL_DIR%" -DCMAKE_BUILD_TYPE="%build_type%" -DBUILD_TESTING:BOOL=False
+:: Note: We disable testing by default. If the intention is for the CI to build and test
+:: this project, then the CI script will turn it back on.
 
-@echo Configuring for build type %build_type% for %build_bitness% bits
-cmake -G "NMake Makefiles"^
-      -DCMAKE_PREFIX_PATH="%IGNITION-MATH_CMAKE_PREFIX_PATH%;"^
-      -DIGNITION-MATH_FOLDER="%IGNITION-MATH_PATH%"^
-      -DCMAKE_INSTALL_PREFIX="install/%build_type%"^
-      -DCMAKE_BUILD_TYPE="%build_type%"^
-      ..
-
-@if %errorlevel% neq 0 exit /b %errorlevel%
-@echo Configuration complete.  To build, run `nmake`
+:: If the caller wants to build and/or install, they should do so after calling this script
