fbc349245bd8ceb3d2cac03d1c144579e369e44b
chapulina <burajiru.no.chapulina@gmail.com>
2018-06-13 10:26:38 -0700

    LocalCache_TEST

--- src/LocalCache_TEST.cc
+++ src/LocalCache_TEST.cc
@@ -24,6 +24,7 @@
 
 #include "ignition/fuel_tools/ClientConfig.hh"
 #include "ignition/fuel_tools/LocalCache.hh"
+#include "ignition/fuel_tools/WorldIdentifier.hh"
 
 #include "test/test_config.h"
 
@@ -39,7 +40,7 @@ using namespace ignition;
 using namespace fuel_tools;
 
 /// \brief Creates a directory structure in the build directory with 6 models
-void createLocal6(ClientConfig &_conf)
+void createLocal6Models(ClientConfig &_conf)
 {
   common::createDirectories("test_cache/localhost:8001/alice/models/am1/2");
   common::createDirectories("test_cache/localhost:8001/alice/models/am2/1");
@@ -77,7 +78,7 @@ void createLocal6(ClientConfig &_conf)
 }
 
 /// \brief Creates a directory structure in the build directory with 3 models
-void createLocal3(ClientConfig &_conf)
+void createLocal3Models(ClientConfig &_conf)
 {
   common::createDirectories("test_cache/localhost:8007/alice/models/am1/2");
   common::createDirectories("test_cache/localhost:8007/bob/models/bm1/1");
@@ -102,6 +103,70 @@ void createLocal3(ClientConfig &_conf)
   _conf.AddServer(srv);
 }
 
+/// \brief Creates a directory structure in the build directory with 6 worlds
+void createLocal6Worlds(ClientConfig &_conf)
+{
+  common::createDirectories("test_cache/localhost:8001/alice/worlds/am1/2");
+  common::createDirectories("test_cache/localhost:8001/alice/worlds/am2/1");
+  common::createDirectories("test_cache/localhost:8001/bob/worlds/bm1/1");
+  common::createDirectories("test_cache/localhost:8001/bob/worlds/bm2/2");
+  common::createDirectories("test_cache/localhost:8001/trudy/worlds/tm1/3");
+  common::createDirectories("test_cache/localhost:8001/trudy/worlds/tm2/2");
+
+  std::ofstream fout(
+      "test_cache/localhost:8001/alice/worlds/am1/2/world.world",
+      std::ofstream::trunc);
+  fout << "<?xml version=\"1.0\"?>";
+  fout.flush();
+  fout.close();
+
+  common::copyFile(
+      "test_cache/localhost:8001/alice/worlds/am1/2/world.world",
+      "test_cache/localhost:8001/alice/worlds/am2/1/world.world");
+  common::copyFile(
+      "test_cache/localhost:8001/alice/worlds/am1/2/world.world",
+      "test_cache/localhost:8001/bob/worlds/bm1/1/world.world");
+  common::copyFile(
+      "test_cache/localhost:8001/alice/worlds/am1/2/world.world",
+      "test_cache/localhost:8001/bob/worlds/bm2/2/world.world");
+  common::copyFile(
+      "test_cache/localhost:8001/alice/worlds/am1/2/world.world",
+      "test_cache/localhost:8001/trudy/worlds/tm1/3/world.world");
+  common::copyFile(
+      "test_cache/localhost:8001/alice/worlds/am1/2/world.world",
+      "test_cache/localhost:8001/trudy/worlds/tm2/2/world.world");
+
+  ignition::fuel_tools::ServerConfig srv;
+  srv.URL("http://localhost:8001/");
+  _conf.AddServer(srv);
+}
+
+/// \brief Creates a directory structure in the build directory with 3 worlds
+void createLocal3Worlds(ClientConfig &_conf)
+{
+  common::createDirectories("test_cache/localhost:8007/alice/worlds/am1/2");
+  common::createDirectories("test_cache/localhost:8007/bob/worlds/bm1/1");
+  common::createDirectories("test_cache/localhost:8007/trudy/worlds/tm1/3");
+
+  std::ofstream fout(
+      "test_cache/localhost:8007/alice/worlds/am1/2/world.world",
+      std::ofstream::trunc);
+  fout << "<?xml version=\"1.0\"?>";
+  fout.flush();
+  fout.close();
+
+  common::copyFile(
+      "test_cache/localhost:8007/alice/worlds/am1/2/world.world",
+      "test_cache/localhost:8007/bob/worlds/bm1/1/world.world");
+  common::copyFile(
+      "test_cache/localhost:8007/alice/worlds/am1/2/world.world",
+      "test_cache/localhost:8007/trudy/worlds/tm1/3/world.world");
+
+  ignition::fuel_tools::ServerConfig srv;
+  srv.URL("http://localhost:8007/");
+  _conf.AddServer(srv);
+}
+
 /////////////////////////////////////////////////
 /// \brief Iterate through all models in cache
 TEST(LocalCache, AllModels)
@@ -111,8 +176,8 @@ TEST(LocalCache, AllModels)
   common::createDirectories("test_cache");
   ClientConfig conf;
   conf.CacheLocation(common::cwd() + "/test_cache");
-  createLocal6(conf);
-  createLocal3(conf);
+  createLocal6Models(conf);
+  createLocal3Models(conf);
 
   ignition::fuel_tools::LocalCache cache(&conf);
 
@@ -138,8 +203,8 @@ TEST(LocalCache, MatchingModels)
   common::createDirectories("test_cache");
   ClientConfig conf;
   conf.CacheLocation(common::cwd() + "/test_cache");
-  createLocal6(conf);
-  createLocal3(conf);
+  createLocal6Models(conf);
+  createLocal3Models(conf);
 
   ignition::fuel_tools::LocalCache cache(&conf);
 
@@ -170,7 +235,7 @@ TEST(LocalCache, MatchingModel)
   common::createDirectories("test_cache");
   ClientConfig conf;
   conf.CacheLocation(common::cwd() + "/test_cache");
-  createLocal6(conf);
+  createLocal6Models(conf);
 
   ignition::fuel_tools::LocalCache cache(&conf);
 
@@ -213,6 +278,118 @@ TEST(LocalCache, MatchingModel)
   bogus3.Owner("trudy");
   bogus3.Name("tm3");
   EXPECT_FALSE(cache.MatchingModel(bogus3));
+}
+
+/////////////////////////////////////////////////
+/// \brief Iterate through all worlds in cache
+TEST(LocalCache, AllWorlds)
+{
+  ASSERT_EQ(0, ChangeDirectory(PROJECT_BINARY_PATH));
+  common::removeAll("test_cache");
+  common::createDirectories("test_cache");
+  ClientConfig conf;
+  conf.CacheLocation(common::cwd() + "/test_cache");
+  createLocal6Worlds(conf);
+  createLocal3Worlds(conf);
+
+  ignition::fuel_tools::LocalCache cache(&conf);
+
+  auto iter = cache.AllWorlds();
+  std::set<std::string> uniqueNames;
+  while (iter)
+  {
+    uniqueNames.insert(iter->UniqueName());
+    ++iter;
+  }
+  EXPECT_EQ(9u, uniqueNames.size());
+
+  EXPECT_NE(uniqueNames.end(), uniqueNames.find(
+      "localhost:8001/alice/worlds/am1"));
+}
+
+/////////////////////////////////////////////////
+/// \brief Get all worlds that match some fields
+TEST(LocalCache, MatchingWorlds)
+{
+  ASSERT_EQ(0, ChangeDirectory(PROJECT_BINARY_PATH));
+  common::removeAll("test_cache");
+  common::createDirectories("test_cache");
+  ClientConfig conf;
+  conf.CacheLocation(common::cwd() + "/test_cache");
+  createLocal6Worlds(conf);
+  createLocal3Worlds(conf);
+
+  ignition::fuel_tools::LocalCache cache(&conf);
+
+  WorldIdentifier am1;
+  am1.SetOwner("alice");
+  am1.SetName("am1");
+  auto iter = cache.MatchingWorlds(am1);
+
+  std::set<std::string> uniqueNames;
+  while (iter)
+  {
+    EXPECT_EQ("alice", iter->Owner());
+    EXPECT_EQ("am1", iter->Name());
+
+    uniqueNames.insert(iter->UniqueName());
+    ++iter;
+  }
+  EXPECT_EQ(2u, uniqueNames.size());
+}
+
+
+/////////////////////////////////////////////////
+/// \brief Get a specific world from cache
+TEST(LocalCache, MatchingWorld)
+{
+  ASSERT_EQ(0, ChangeDirectory(PROJECT_BINARY_PATH));
+  common::removeAll("test_cache");
+  common::createDirectories("test_cache");
+  ClientConfig conf;
+  conf.CacheLocation(common::cwd() + "/test_cache");
+  createLocal6Worlds(conf);
+
+  ignition::fuel_tools::LocalCache cache(&conf);
+
+  ignition::fuel_tools::ServerConfig srv1;
+  srv1.URL("http://localhost:8001/");
+
+  ignition::fuel_tools::ServerConfig srv2;
+  srv2.URL("http://localhost:8002/");
+
+  WorldIdentifier am1;
+  am1.SetServer(srv1);
+  am1.SetOwner("alice");
+  am1.SetName("am1");
+  EXPECT_TRUE(cache.MatchingWorld(am1));
+  EXPECT_EQ("alice", am1.Owner());
+  EXPECT_EQ("am1", am1.Name());
+  EXPECT_EQ("http://localhost:8001", am1.Server().URL());
+
+  WorldIdentifier tm2;
+  tm2.SetServer(srv1);
+  tm2.SetOwner("trudy");
+  tm2.SetName("tm2");
+  EXPECT_TRUE(cache.MatchingWorld(tm2));
+
+  WorldIdentifier bogus1;
+  bogus1.SetServer(srv2);
+  bogus1.SetOwner("trudy");
+  bogus1.SetName("tm2");
+  EXPECT_FALSE(cache.MatchingWorld(bogus1));
+
+  WorldIdentifier bogus2;
+  bogus2.SetServer(srv1);
+  bogus2.SetOwner("tfudy");
+  bogus2.SetName("tm2");
+  EXPECT_FALSE(cache.MatchingWorld(bogus2));
+
+  WorldIdentifier bogus3;
+  bogus3.SetServer(srv1);
+  bogus3.SetOwner("trudy");
+  bogus3.SetName("tm3");
+  EXPECT_FALSE(cache.MatchingWorld(bogus3));
 }
 
 //////////////////////////////////////////////////
