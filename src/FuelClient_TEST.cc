2bca270746b7784855a2f24f745b0f69eb8571b4
chapulina <burajiru.no.chapulina@gmail.com>
2020-03-16 20:27:53 -0700

    more test fixes

--- src/FuelClient_TEST.cc
+++ src/FuelClient_TEST.cc
@@ -1211,13 +1211,7 @@ TEST_F(FuelClientTest, Models)
   {
     serverConfig.Clear();
     ModelIter const iter = client.Models(serverConfig);
-    // If you have models in the cache location, such as ~/.ignition/fuel,
-    // then the model iter will be true. On CI systems, which probably
-    // have an empty cache, the model iter will be false.
-    if (common::exists(client.Config().CacheLocation()))
-      EXPECT_TRUE(iter);
-    else
-      EXPECT_FALSE(iter);
+    EXPECT_FALSE(iter);
   }
 }
 
