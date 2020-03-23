6d9b9b90d4e664ff70672102afbdbeab6bcabcb0
Steve Peters <scpeters@openrobotics.org>
2020-03-23 13:20:38 -0700

    use Silvio's branch to test FindZip improvements

--- configure.bat
+++ configure.bat
@@ -9,7 +9,8 @@ call %win_lib% :download_unzip_install jsoncpp-1.8.4-vc15-x64-dll-MD.zip
 call %win_lib% :download_unzip_install libyaml-0.1.7-vc15-x64-md.zip
 call %win_lib% :download_unzip_install libzip-1.4.0_zlip-1.2.11_vc15-x64-dll-MD.zip
 call %win_lib% :install_ign_project ign-common ign-common3
-call %win_lib% :install_ign_project ign-msgs fuel_resource_metadata
+call %win_lib% :install_ign_project ign-msgs ign-msgs5
+call %win_lib% :install_ign_project ign-cmake Silvio-Traversaro/fix-use-of-findzip-without-pkgconfig-1584891888504
 
 :: Set configuration variables
 @set build_type=Release
