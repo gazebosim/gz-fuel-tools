8aed84d916d6c64e567a4d63a30f768dea43c974
Louise Poubel <louise@openrobotics.org>
2020-07-20 09:14:16 -0700

    LD_LIBRARY_PATH
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- .github/ci-bionic/after_make.sh
+++ .github/ci-bionic/after_make.sh
@@ -7,6 +7,7 @@ make install
 
 # So ign-tools finds fuel-tools
 export IGN_CONFIG_PATH=/usr/local/share/ignition
+export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH
 
 # Compile example
 cd ../example
