ad782403b48ffc8a1c33a61015a0d62c8dce27d6
ahcorde <ahcorde@gmail.com>
2021-04-14 09:03:07 +0200

    Improved windows support
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- src/CollectionIdentifier.cc
+++ src/CollectionIdentifier.cc
@@ -71,22 +71,9 @@ CollectionIdentifier::~CollectionIdentifier() = default;
 //////////////////////////////////////////////////
 std::string CollectionIdentifier::UniqueName() const
 {
-  std::vector<std::string> stringList = {this->dataPtr->server.Url().Str(),
-    this->dataPtr->owner, "collections", this->dataPtr->name};
-
-  // This loop is to avoid duplicated '/' and '/' at the end of the string
-  std::string result;
-  for (const auto &s : stringList)
-  {
-    std::string tmp = common::joinPaths(result, s);
-    if (tmp[0] == '/')
-      tmp.erase(0, 1);
-    if(tmp[tmp.length()-1] == '/')
-      tmp.erase(tmp.length()-1, 1);
-    result = tmp;
-  }
-
-  return result;
+  return common::joinPaths(this->dataPtr->server.Url().Str(),
+                           this->dataPtr->owner, "collections",
+                           this->dataPtr->name);
 }
 
 //////////////////////////////////////////////////
