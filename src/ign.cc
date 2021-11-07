gazebosim/gz-fuel-tools

commit c680ee883ecb9829a56904c6ad0b30cfeb84aba0

APIs for retrieving models in parallel (#199)
* APIs for retrieving models in parallel

Signed-off-by: Michael Carroll <michael@openrobotics.org>

* Lint

Signed-off-by: Michael Carroll <michael@openrobotics.org>

* Update to use parallel download models and worlds

Signed-off-by: Michael Carroll <michael@openrobotics.org>

* Alternative implementation using threads

Signed-off-by: Michael Carroll <michael@openrobotics.org>

* Uncommited local changes for hash and linting

Signed-off-by: Michael Carroll <michael@openrobotics.org>

* fix codecheck

Signed-off-by: Nate Koenig <nate@openrobotics.org>

* Added missing insertion

Signed-off-by: Nate Koenig <nate@openrobotics.org>

* Correctly handle local dependencies by skipping

Signed-off-by: Michael Carroll <michael@openrobotics.org>

Co-authored-by: Nate Koenig <nate@openrobotics.org>

src/ign.cc
@@ -630,93 +630,17 @@ extern "C" IGNITION_FUEL_TOOLS_VISIBLE int downloadUrl(const char *_url,
       return false;
     }
 
-    size_t jobs = std::max(1, _jobs);
-
-    ignmsg << "Using " << jobs << " jobs to download collection of "
-           << totalItemCount << " items" << std::endl;
-
-    std::deque<std::future<ignition::fuel_tools::Result>> tasks;
-
-    // Check for finished tasks by checking if the status of their futures is
-    // "ready". If a task is finished, check if it succeeded and print out an
-    // error message if it failed. When a task is finished, it gets erased from
-    // the tasks list to make room for other tasks to be added.
-    size_t itemCount = 0;
-    auto checkForFinishedTasks = [&itemCount, &totalItemCount, &tasks] {
-      auto finishedIt =
-          std::partition(tasks.begin(), tasks.end(), [](const auto &_task)
-              {
-                return std::future_status::ready !=
-                _task.wait_for(std::chrono::milliseconds(100));
-              });
-
-      if (finishedIt != tasks.end())
-      {
-        for (auto taskIt = finishedIt; taskIt != tasks.end(); ++taskIt)
-        {
-          ignition::fuel_tools::Result result = taskIt->get();
-          if (result)
-          {
-            ++itemCount;
-          }
-          else
-          {
-            ignerr << result.ReadableResult() << std::endl;
-          }
-        }
-
-        tasks.erase(finishedIt, tasks.end());
-        ignmsg << "Downloaded: " << itemCount << " / " << totalItemCount
-               << std::endl;
-      }
-    };
-
-    // Here we use std::async to download items in parallel. The download task
-    // is started asynchronously and gets added to the task list which is
-    // monitored for completion.
     if (downloadModels)
     {
-      for (const auto &modelId : modelIds)
-      {
-        // Check if any of the tasks are done. Don't start a new task until the
-        // number of tasks in the tasks lists is below the number of jobs
-        // specified by the user.
-        while (tasks.size() >= jobs)
-        {
-          checkForFinishedTasks();
-        }
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-        auto handle = std::async(std::launch::async, [&modelId, &client]
-            {
-              return client.DownloadModel(modelId);
-            });
-        tasks.push_back(std::move(handle));
-      }
+      auto result = client.DownloadModels(modelIds, _jobs);
     }
 
     if (downloadWorlds)
     {
-      // We need a mutable worldId because DownloadWorld modifies it
-      for (auto &worldId : worldIds)
-      {
-        // Check if any of the tasks are done
-        while (tasks.size() >= jobs)
-        {
-          checkForFinishedTasks();
-        }
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-        auto handle = std::async(std::launch::async, [&worldId, &client]
-            {
-            return client.DownloadWorld(worldId);
-            });
-        tasks.push_back(std::move(handle));
-      }
-    }
-
-    // All the tasks have been queued. Now wait for them to finish
-    while (!tasks.empty())
-    {
-      checkForFinishedTasks();
+      auto result = client.DownloadWorlds(worldIds, _jobs);
+      ignerr << "Failed to download worlds for collection ["
+          << collection.Name()
+          << "]" << std::endl;
     }
   }
   else

include/ignition/fuel_tools/FuelClient.hh
@@ -20,6 +20,7 @@
 
 #include <memory>
 #include <string>
+#include <tuple>
 #include <vector>
 #include <ignition/common/URI.hh>
 
@@ -203,6 +204,31 @@ namespace ignition
       public: Result DownloadModel(const ModelIdentifier &_id,
                   const std::vector<std::string> &_headers);
 
+      /// \brief Download a model from ignition fuel. This will override an
+      /// existing local copy of the model.
+      /// \param[in] _id The model identifier.
+      /// \param[in] _headers Headers to set on the HTTP request.
+      /// \param[out] _dependencies List of models that this model depends on.
+      /// \return Result of the download operation
+      public: Result DownloadModel(const ModelIdentifier &_id,
+                  const std::vector<std::string> &_headers,
+                  std::vector<ModelIdentifier> &_dependencies);
+
+      /// \brief Retrieve the list of dependencies for a model.
+      /// \param[in] _id The model identifier.
+      /// \param[out] _dependencies The list of dependencies.
+      /// \return Result of the operation
+      public: Result ModelDependencies(const ModelIdentifier &_id,
+                  std::vector<ModelIdentifier> &_dependencies);
+
+      /// \brief Retrieve the list of dependencies for a list of models.
+      /// \param[in] _id The list of model identifiers.
+      /// \param[out] _dependencies The list of dependencies.
+      /// \return Result of the operation
+      public: Result ModelDependencies(
+                  const std::vector<ModelIdentifier> &_id,
+                  std::vector<ModelIdentifier> &_dependencies);
+
       /// \brief Download a world from Ignition Fuel. This will override an
       /// existing local copy of the world.
       /// \param[out] _id The world identifier, with local path updated.
@@ -226,6 +252,28 @@ namespace ignition
       /// \return Result of the download operation.
       public: Result DownloadWorld(const common::URI &_worldUrl,
                                    std::string &_path);
+
+      using ModelResult = std::tuple<ModelIdentifier, Result>;
+
+      /// \brief Download a list of models from ignition fuel.
+      /// \param[in] _ids The list of model ids to download.
+      ///   This will also find all recursive dependencies of the models
+      /// \param[in] _jobs Number of parallel jobs to use to download models
+      /// \return Result of the download operation.
+      //    The resulting vector will be at least the size of the _ids input
+      //    vector, but may be larger depending on the number of depedencies
+      //    downloaded
+      public: std::vector<ModelResult> DownloadModels(
+                  const std::vector<ModelIdentifier> &_ids,
+                  size_t _jobs = 2);
+
+      /// \brief Download a list of mworlds from ignition fuel.
+      /// \param[in] _ids The list of world ids to download.
+      /// \param[in] _jobs Number of parallel jobs to use to download worlds.
+      /// \return Result of the download operation.
+      public: Result DownloadWorlds(
+                  const std::vector<WorldIdentifier> &_ids,
+                  size_t _jobs = 2);
 
       /// \brief Check if a model is already present in the local cache.
       /// \param[in] _modelUrl The unique URL of the model on a Fuel server.

src/ClientConfig_TEST.cc
@@ -64,7 +64,6 @@ std::string homePath()
 /// \ToDo: Move this function to ignition::common::Filesystem
 std::string cachePath()
 {
-  std::string cachePath;
 #ifndef _WIN32
   return std::string("/tmp/ignition/fuel");
 #else

src/FuelClient.cc
@@ -565,6 +565,28 @@ Result FuelClient::DownloadModel(const ModelIdentifier &_id)
 Result FuelClient::DownloadModel(const ModelIdentifier &_id,
     const std::vector<std::string> &_headers)
 {
+  std::vector<ModelIdentifier> dependencies;
+  auto res = this->DownloadModel(_id, _headers, dependencies);
+
+  if(!res)
+    return res;
+
+  for (auto dep : dependencies)
+  {
+    auto dep_res = this->DownloadModel(dep, _headers);
+
+    if(!dep_res)
+      return dep_res;
+  }
+
+  return res;
+}
+
+//////////////////////////////////////////////////
+Result FuelClient::DownloadModel(const ModelIdentifier &_id,
+    const std::vector<std::string> &_headers,
+    std::vector<ModelIdentifier> &_dependencies)
+{
   // Server config
   if (!_id.Server().Url().Valid() || _id.Server().Version().empty())
   {
@@ -626,10 +648,19 @@ Result FuelClient::DownloadModel(const ModelIdentifier &_id,
   if (!this->dataPtr->cache->SaveModel(newId, resp.data, true))
     return Result(ResultType::FETCH_ERROR);
 
+  return this->ModelDependencies(_id, _dependencies);
+}
+
+//////////////////////////////////////////////////
+Result FuelClient::ModelDependencies(const ModelIdentifier &_id,
+    std::vector<ModelIdentifier> &_dependencies)
+{
+  _dependencies.clear();
+
   // Locate any dependencies from the input model and download them.
   std::string path;
   ignition::msgs::FuelMetadata meta;
-  if (this->CachedModel(ignition::common::URI(newId.UniqueName()), path))
+  if (this->CachedModel(ignition::common::URI(_id.UniqueName()), path))
   {
     std::string metadataPath =
       ignition::common::joinPaths(path, "metadata.pbtxt");
@@ -664,19 +695,60 @@ Result FuelClient::DownloadModel(const ModelIdentifier &_id,
 
       for (int i = 0; i < meta.dependencies_size(); ++i)
       {
-        std::string dependencyPath;
         ignition::common::URI dependencyURI(meta.dependencies(i).uri());
 
-        // If the model is not already cached, download it; this prevents
-        // any sort of cyclic dependencies from running infinitely
-        if (!this->CachedModel(dependencyURI, dependencyPath))
-          this->DownloadModel(dependencyURI, dependencyPath);
+        ModelIdentifier dependencyID;
+        if(!this->ParseModelUrl(dependencyURI, dependencyID))
+        {
+          // There is a potential that depdencies are specified via
+          // [model://model_name], which is valid, but not something that we
+          // can fetch from Fuel. In that case, warn the user so they have
+          // a chance to update their specified dependencies.
+          ignwarn << "Error resolving URL for dependency [" <<
+            meta.dependencies(i).uri() << "] of model [" <<
+            _id.UniqueName() <<"]: Skipping" << std::endl;
+        } else {
+          _dependencies.push_back(dependencyID);
+        }
       }
     }
   }
 
   return Result(ResultType::FETCH);
 }
+
+//////////////////////////////////////////////////
+Result FuelClient::ModelDependencies(
+    const std::vector<ModelIdentifier> &_ids,
+    std::vector<ModelIdentifier> &_dependencies)
+{
+  std::vector<ModelIdentifier> newDeps;
+  for (auto modelId : _ids)
+  {
+    std::vector<ModelIdentifier> modelDeps;
+    auto result = this->ModelDependencies(modelId, modelDeps);
+
+    if (!modelDeps.empty())
+    {
+      std::vector<ModelIdentifier> recursiveDeps;
+      this->ModelDependencies(modelDeps, recursiveDeps);
+
+      for (auto dep : modelDeps)
+      {
+        newDeps.push_back(dep);
+      }
+
+      for (auto dep : recursiveDeps)
+      {
+        newDeps.push_back(dep);
+      }
+    }
+  }
+
+  _dependencies = std::vector<ModelIdentifier>(newDeps.begin(), newDeps.end());
+  return Result(ResultType::FETCH);
+}
+
 
 //////////////////////////////////////////////////
 Result FuelClient::DownloadWorld(WorldIdentifier &_id)
@@ -741,6 +813,176 @@ Result FuelClient::DownloadWorld(WorldIdentifier &_id)
   // Save
   if (!this->dataPtr->cache->SaveWorld(_id, resp.data, true))
     return Result(ResultType::FETCH_ERROR);
+
+  return Result(ResultType::FETCH);
+}
+
+//////////////////////////////////////////////////
+namespace std
+{
+  template<> struct hash<ModelIdentifier>
+  {
+    std::size_t operator()(const ModelIdentifier &_id) const noexcept
+    {
+      return std::hash<std::string>{}(_id.AsString());
+    }
+  };
+}
+
+//////////////////////////////////////////////////
+std::vector<FuelClient::ModelResult> FuelClient::DownloadModels(
+    const std::vector<ModelIdentifier> &_ids,
+    size_t _jobs)
+{
+  std::mutex resultMutex;
+  std::vector<FuelClient::ModelResult> result;
+
+  std::mutex idsMutex;
+  std::deque<ModelIdentifier> idsToDownload(_ids.begin(), _ids.end());
+  std::unordered_set<ModelIdentifier> uniqueIds(_ids.begin(), _ids.end());
+
+  std::atomic<bool> running = true;
+
+  auto downloadWorker = [&](){
+    ModelIdentifier id;
+
+    while(running)
+    {
+      // Pop the next ID off the queue
+      {
+        std::lock_guard<std::mutex> lock(idsMutex);
+
+        if (idsToDownload.empty())
+        {
+          std::this_thread::sleep_for(std::chrono::milliseconds(10));
+          continue;
+        }
+
+        id = idsToDownload.front();
+        idsToDownload.pop_front();
+      }
+
+      std::vector<ModelIdentifier> dependencies;
+      auto modelResult = this->DownloadModel(id, {}, dependencies);
+
+      {
+        std::lock_guard<std::mutex> lock(resultMutex);
+        result.push_back(std::make_tuple(id, modelResult));
+      }
+
+      if (!dependencies.empty())
+      {
+        std::lock_guard<std::mutex> lock(idsMutex);
+        igndbg << "Adding " << dependencies.size()
+          << " model dependencies to queue from " << id.Name() << "\n";
+        for (auto dep : dependencies)
+        {
+          if (uniqueIds.count(dep) == 0)
+          {
+            idsToDownload.push_back(dep);
+            uniqueIds.insert(dep);
+          }
+        }
+      }
+    }
+  };
+
+  std::vector<std::thread> workers;
+
+  for (size_t ii = 0; ii < _jobs; ++ii)
+  {
+    workers.push_back(std::thread(downloadWorker));
+  }
+
+  ignmsg << "Preparing to download "
+    << idsToDownload.size() << " models with "
+    << _jobs << " worker threads\n";
+
+
+  while (running)
+  {
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+    if(idsToDownload.empty())
+    {
+      running = false;
+    }
+  }
+
+  for (auto& worker : workers)
+  {
+    worker.join();
+  }
+
+  ignmsg << "Finished, downloaded " << result.size() << " models in total\n";
+
+  return result;
+}
+
+//////////////////////////////////////////////////
+Result FuelClient::DownloadWorlds(
+    const std::vector<WorldIdentifier> &_ids, size_t _jobs)
+{
+  std::deque<std::future<ignition::fuel_tools::Result>> tasks;
+  // Check for finished tasks by checking if the status of their futures is
+  // "ready". If a task is finished, check if it succeeded and print out an
+  // error message if it failed. When a task is finished, it gets erased from
+  // the tasks list to make room for other tasks to be added.
+  size_t itemCount = 0;
+  const size_t totalItemCount = _ids.size();
+
+  ignmsg << "Using " << _jobs << " jobs to download collection of "
+         << totalItemCount << " items" << std::endl;
+
+  auto checkForFinishedTasks = [&itemCount, &totalItemCount, &tasks] {
+    auto finishedIt =
+        std::partition(tasks.begin(), tasks.end(), [](const auto &_task)
+            {
+              return std::future_status::ready !=
+              _task.wait_for(std::chrono::milliseconds(100));
+            });
+
+    if (finishedIt != tasks.end())
+    {
+      for (auto taskIt = finishedIt; taskIt != tasks.end(); ++taskIt)
+      {
+        ignition::fuel_tools::Result result = taskIt->get();
+        if (result)
+        {
+          ++itemCount;
+        }
+        else
+        {
+          ignerr << result.ReadableResult() << std::endl;
+        }
+      }
+
+      tasks.erase(finishedIt, tasks.end());
+      ignmsg << "Downloaded: " << itemCount << " / " << totalItemCount
+             << std::endl;
+    }
+  };
+
+  // We need a mutable worldId because DownloadWorld modifies it
+  for (auto& id : _ids)
+  {
+    while (tasks.size() >= _jobs)
+    {
+      checkForFinishedTasks();
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    auto handle = std::async(std::launch::async, [&id, this]
+        {
+          WorldIdentifier tempId = id;
+          return this->DownloadWorld(tempId);
+        });
+    tasks.push_back(std::move(handle));
+  }
+
+  while (!tasks.empty())
+  {
+    checkForFinishedTasks();
+  }
 
   return Result(ResultType::FETCH);
 }

src/FuelClient_TEST.cc
@@ -593,6 +593,80 @@ TEST_F(FuelClientTest, DownloadModel)
 /////////////////////////////////////////////////
 // Windows doesn't support colons in filenames
 // https://github.com/ignitionrobotics/ign-fuel-tools/issues/106
+TEST_F(FuelClientTest, IGN_UTILS_TEST_DISABLED_ON_WIN32(ModelDependencies))
+{
+  // Configure to use binary path as cache
+  ASSERT_EQ(0, ChangeDirectory(PROJECT_BINARY_PATH));
+  common::removeAll("test_cache");
+  common::createDirectories("test_cache");
+  ClientConfig config;
+  config.SetCacheLocation(common::joinPaths(common::cwd(), "test_cache"));
+
+  // Create client
+  FuelClient client(config);
+  EXPECT_EQ(config.CacheLocation(), client.Config().CacheLocation());
+
+  // Download model with a dependency specified within its `metadata.pbtxt`
+  {
+    common::URI url{
+        "https://fuel.ignitionrobotics.org/1.0/JShep1/models/hatchback_red_1"};
+    common::URI depUrl{
+        "https://fuel.ignitionrobotics.org/1.0/JShep1/models/hatchback_1"};
+
+    ModelIdentifier id;
+    ModelIdentifier depId;
+
+    ASSERT_TRUE(client.ParseModelUrl(url, id));
+    ASSERT_TRUE(client.ParseModelUrl(depUrl, depId));
+
+    // Check it is not cached
+    std::string cachedPath;
+    Result res1 = client.CachedModel(url, cachedPath);
+    EXPECT_FALSE(res1);
+    EXPECT_EQ(Result(ResultType::FETCH_ERROR), res1);
+
+    // Check the dependency is not cached
+    Result res2 = client.CachedModel(depUrl, cachedPath);
+    EXPECT_FALSE(res2);
+    EXPECT_EQ(Result(ResultType::FETCH_ERROR), res2);
+
+    // Download on the model, do not download dependencies
+    {
+      std::vector<ModelIdentifier> dependencies;
+      Result res3 = client.DownloadModel(id, {}, dependencies);
+      EXPECT_TRUE(res3);
+      EXPECT_EQ(Result(ResultType::FETCH_ALREADY_EXISTS), res3);
+      EXPECT_EQ(1u, dependencies.size());
+    }
+
+    // Check that the model is cached
+    {
+      Result res4 = client.CachedModel(url, cachedPath);
+      EXPECT_TRUE(res4);
+      EXPECT_EQ(Result(ResultType::FETCH_ALREADY_EXISTS), res4);
+    }
+
+    // Check the dependency is not cached
+    {
+      Result res5 = client.CachedModel(depUrl, cachedPath);
+      EXPECT_FALSE(res5);
+      EXPECT_EQ(Result(ResultType::FETCH_ERROR), res5);
+    }
+
+    // Check that the dependencies are populated
+    {
+      std::vector<ModelIdentifier> dependencies;
+      Result res6 = client.ModelDependencies(id, dependencies);
+      EXPECT_TRUE(res6);
+      EXPECT_EQ(1u, dependencies.size());
+    }
+  }
+}
+
+
+/////////////////////////////////////////////////
+// Windows doesn't support colons in filenames
+// https://github.com/ignitionrobotics/ign-fuel-tools/issues/106
 TEST_F(FuelClientTest, CachedModel)
 {
   // Configure to use binary path as cache and populate it

commit 821f8daf037a2692720b7085649f3f13979cb850

ign.cc: remove console error message
This was introduced in #199.

Signed-off-by: Steve Peters <scpeters@openrobotics.org>

src/ign.cc
@@ -638,9 +638,6 @@ extern "C" IGNITION_FUEL_TOOLS_VISIBLE int downloadUrl(const char *_url,
     if (downloadWorlds)
     {
       auto result = client.DownloadWorlds(worldIds, _jobs);
-      ignerr << "Failed to download worlds for collection ["
-          << collection.Name()
-          << "]" << std::endl;
     }
   }
   else