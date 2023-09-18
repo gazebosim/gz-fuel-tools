3601bfeca1bf4d0048f226e802de4b37ceba51e9
Michael Carroll <mjcarroll@intrinsic.ai>
2023-09-18 22:34:57 +0000

    Remove deprecations
    Signed-off-by: Michael Carroll <mjcarroll@intrinsic.ai>

--- src/ClientConfig.cc
+++ src/ClientConfig.cc
@@ -208,20 +208,10 @@ std::string ServerConfig::AsPrettyString(const std::string &_prefix) const
 //////////////////////////////////////////////////
 ClientConfig::ClientConfig() : dataPtr(new ClientConfigPrivate)
 {
-  std::string gzFuelPath = "";
+  std::string gzFuelPath;
   if (!gz::common::env("GZ_FUEL_CACHE_PATH", gzFuelPath))
   {
-    // TODO(CH3): Deprecated. Remove on tock.
-    if (gz::common::env("IGN_FUEL_CACHE_PATH", gzFuelPath))
-    {
-      gzwarn << "Using deprecated environment variable [IGN_FUEL_CACHE_PATH] "
-             << "to set cache path. Please use [GZ_FUEL_CACHE_PATH] instead."
-             << std::endl;
-    }
-    else
-    {
-      return;
-    }
+    return;
   }
 
   if (!gz::common::isDirectory(gzFuelPath))
@@ -270,7 +260,7 @@ bool ClientConfig::LoadConfig(const std::string &_file)
 
 
   FILE *fh = fopen(_file.c_str(), "r");
-  if (!fh)
+  if (fh == nullptr)
   {
     gzerr << "Failed to open configuration file ["
       << _file << "]" << std::endl;
@@ -442,23 +432,11 @@ bool ClientConfig::LoadConfig(const std::string &_file)
     cacheLocation = cacheLocationConfig;
 
   // Do not overwrite the cache location if GZ_FUEL_CACHE_PATH is set.
-  std::string gzFuelPath = "";
+  std::string gzFuelPath;
   if (gz::common::env("GZ_FUEL_CACHE_PATH", gzFuelPath))
   {
     gzwarn << "GZ_FUEL_CACHE_PATH is set to [" << gzFuelPath << "]. The "
             << "path in the configuration file will be ignored" << std::endl;
-    cacheLocation = gzFuelPath;
-  }
-  // TODO(CH3): Deprecated. Remove on tock.
-  else if (gz::common::env("IGN_FUEL_CACHE_PATH", gzFuelPath))
-  {
-    gzwarn << "Using deprecated environment variable [IGN_FUEL_CACHE_PATH] "
-           << "to set cache path. Please use [GZ_FUEL_CACHE_PATH] instead."
-           << std::endl;
-
-    gzwarn << "GZ_FUEL_CACHE_PATH is set to [" << gzFuelPath << "]. The "
-            << "path in the configuration file will be ignored" << std::endl;
-
     cacheLocation = gzFuelPath;
   }
   this->SetCacheLocation(cacheLocation);
