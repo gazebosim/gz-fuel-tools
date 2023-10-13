364aade87a68748f6ffeb1b3390514965535b024
Michael Carroll <mjcarroll@intrinsic.ai>
2023-10-13 10:11:22 -0500

    Split ServerConfig and ClientConfig across multiple files (#380)
    Signed-off-by: Michael Carroll <mjcarroll@intrinsic.ai>

--- include/gz/fuel_tools/ClientConfig.hh
+++ include/gz/fuel_tools/ClientConfig.hh
@@ -25,6 +25,7 @@
 #include <gz/common/URI.hh>
 
 #include "gz/fuel_tools/Export.hh"
+#include "gz/fuel_tools/ServerConfig.hh"
 
 #ifdef _WIN32
 // Disable warning C4251 which is triggered by
@@ -37,72 +38,8 @@ namespace gz
 {
   namespace fuel_tools
   {
-    /// \brief forward declaration
-    class ServerConfigPrivate;
-
     /// \brief Forward Declaration
     class ClientConfigPrivate;
-
-    /// \brief Describes options needed for a server.
-    class GZ_FUEL_TOOLS_VISIBLE ServerConfig
-    {
-      /// \brief Constructor.
-      public: ServerConfig();
-
-      /// \brief Copy constructor.
-      /// \param[in] _orig The server config to copy.
-      public: ServerConfig(const ServerConfig &_orig);
-
-      /// \brief Assignment operator overload.
-      /// \param[in] _orig The server config to copy.
-      public: ServerConfig &operator=(const ServerConfig &_orig);
-
-      /// \brief Destructor.
-      public: ~ServerConfig();
-
-      /// \brief Clear the server config. This will set all values to empty
-      /// strings, except the version string which will be set to its default
-      /// value.
-      public: void Clear();
-
-      /// \brief Get the URL to access the server.
-      /// \return The URL of this server.
-      public: common::URI Url() const;
-
-      /// \brief Set the URL of this server.
-      /// \param[in] _url URL of this server.
-      public: void SetUrl(const common::URI &_url);
-
-      /// \brief Get the API key to auth with the server.
-      /// \return The API key.
-      public: std::string ApiKey() const;
-
-      /// \brief Set the API key to auth with the server.
-      /// \param[in] _key The API key.
-      public: void SetApiKey(const std::string &_key);
-
-      /// \brief Get the protocol version used with this server.
-      /// \return The version. E.g.: "1.0".
-      public: std::string Version() const;
-
-      /// \brief Set the protocol version used with this server.
-      /// \param[in] _version The version. E.g.: "1.0".
-      public: void SetVersion(const std::string &_version);
-
-      /// \brief Returns all the server information as a string.
-      /// \param[in] _prefix Optional prefix for every line of the string.
-      /// \return Server information string
-      public: std::string AsString(const std::string &_prefix = "") const;
-
-      /// \brief Returns all the available model information as a string using
-      /// colors for better human parsing.
-      /// \param[in] _prefix Optional prefix for every line of the string.
-      /// \return Model information string
-      public: std::string AsPrettyString(const std::string &_prefix = "") const;
-
-      /// \brief PIMPL
-      private: std::unique_ptr<ServerConfigPrivate> dataPtr;
-    };
 
     /// \brief High level interface to Gazebo Fuel.
     ///
---
+++ src/ServerConfig_TEST.cc
@@ -0,0 +1,123 @@
+/*
+ * Copyright (C) 2017 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <gtest/gtest.h>
+#include <fstream>
+#include <string>
+#include <gz/common/Console.hh>
+#include <gz/common/Filesystem.hh>
+#include <gz/common/TempDirectory.hh>
+#include <gz/common/testing/TestPaths.hh>
+#include <gz/common/Util.hh>
+
+#include "gz/fuel_tools/ServerConfig.hh"
+
+using namespace gz;
+using namespace fuel_tools;
+
+/////////////////////////////////////////////////
+/// \brief Get home directory.
+/// \return Home directory or empty string if home wasn't found.
+/// \ToDo: Move this function to gz::common::Filesystem
+std::string homePath()
+{
+  std::string homePath;
+#ifndef _WIN32
+  gz::common::env("HOME", homePath);
+#else
+  gz::common::env("USERPROFILE", homePath);
+#endif
+  return homePath;
+}
+
+/////////////////////////////////////////////////
+class ServerConfigTest: public ::testing::Test
+{
+  public: void SetUp() override
+  {
+    gz::common::Console::SetVerbosity(4);
+    tempDir = gz::common::testing::MakeTestTempDirectory();
+    ASSERT_TRUE(tempDir->Valid()) << tempDir->Path();
+
+    gz::common::chdir(tempDir->Path());
+  }
+
+  public: std::string cachePath()
+  {
+    return this->tempDir->Path();
+  }
+
+  public: std::shared_ptr<gz::common::TempDirectory> tempDir;
+};
+
+/////////////////////////////////////////////////
+TEST_F(ServerConfigTest, ApiKey)
+{
+  ServerConfig config;
+  EXPECT_TRUE(config.ApiKey().empty());
+
+  config.SetApiKey("my_api_key");
+  EXPECT_EQ("my_api_key", config.ApiKey());
+
+  config.SetApiKey("my_other_api_key");
+  EXPECT_EQ("my_other_api_key", config.ApiKey());
+}
+
+/////////////////////////////////////////////////
+TEST_F(ServerConfigTest, Url)
+{
+  // Invalid URL string
+  {
+    ServerConfig srv;
+    srv.SetUrl(common::URI("asdf"));
+    EXPECT_TRUE(srv.Url().Str().empty());
+  }
+
+  // Valid URL
+  {
+    ServerConfig srv;
+    srv.SetUrl(common::URI("http://banana:8080"));
+    EXPECT_EQ("http://banana:8080", srv.Url().Str());
+    EXPECT_EQ("http", srv.Url().Scheme());
+    EXPECT_EQ("banana:8080", srv.Url().Path().Str());
+    EXPECT_FALSE(srv.Url().Authority());
+  }
+
+  // Trailing /
+  {
+    ServerConfig srv;
+    srv.SetUrl(common::URI("http://banana:8080"));
+    EXPECT_EQ("http://banana:8080", srv.Url().Str());
+    EXPECT_EQ("http", srv.Url().Scheme());
+    EXPECT_EQ("banana:8080", srv.Url().Path().Str());
+    EXPECT_FALSE(srv.Url().Authority());
+  }
+
+  // Set from URI
+  {
+    auto url = common::URI();
+    url.SetScheme("http");
+    url.Path() = common::URIPath("banana:8080");
+
+    ServerConfig srv;
+    srv.SetUrl(url);
+    EXPECT_EQ("http://banana:8080", srv.Url().Str());
+    EXPECT_EQ("http", srv.Url().Scheme());
+    EXPECT_EQ("banana:8080", srv.Url().Path().Str());
+    EXPECT_FALSE(srv.Url().Authority());
+  }
+}
