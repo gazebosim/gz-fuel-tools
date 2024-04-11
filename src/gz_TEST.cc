1ff983fda799da1039e7a33a296240163ad5d0a5
Jose Luis Rivero <jrivero@osrfoundation.org>
2024-04-11 18:31:29 +0200

    Do not duplicate version for gz_TEST
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- src/gz_TEST.cc
+++ src/gz_TEST.cc
@@ -47,7 +47,7 @@ std::string custom_exec_str(std::string _cmd)
   return result;
 }
 
-auto g_version = std::string(strdup(GZ_FUEL_TOOLS_VERSION_FULL));
+auto g_version = std::string(GZ_FUEL_TOOLS_VERSION_FULL);
 auto g_exec = std::string(GZ_PATH);
 auto g_listCmd = g_exec + " fuel list -v 4 --force-version " + g_version;
 
